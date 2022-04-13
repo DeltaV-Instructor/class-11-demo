fa69edbd0c51a502b1813dc12be991c612e5d352
tektechnologies <info@tektechnologies.com>
2022-04-12 19:18:00 -0500

    add local storage

--- js/app.js
+++ js/app.js
@@ -16,14 +16,73 @@ let uniqueImageCount = 4;
 let newPicsToShow = [];
 
 //constructor function
-function Pizza(name, src){
+function Pizza(name, src, clickedOn, views){
   this.name = name;
   this.src = src;
-  this.views = 0;
-  this.clickedOn = 0;
+  // this.views = 0;
+  // this.clickedOn = 0;
+  if(clickedOn){
+    this.clickedOn = clickedOn;
+  } else {
+    this.clickedOn = 0;
+  }
+  if(views){
+    this.views = views;
+  } else {
+    this.views = 0;
+  }
 
   Pizza.allPizzasArray.push(this);
 }
+
+
+let savedPizzaString = localStorage.getItem('savedPizza');
+console.log('this is the objects in string form ', savedPizzaString);
+
+
+if(savedPizzaString){
+  // parse our string into object
+  let arrayOfNotPizzaObject = JSON.parse(savedPizzaString);
+  console.log('if condition what is our type ',arrayOfNotPizzaObject);
+  //once we have object we are going to run them through our constructor function so that they are Pizza objects.
+  for(let j = 0; j < arrayOfNotPizzaObject.length; j++){
+    new Pizza(
+      arrayOfNotPizzaObject[j].name,
+      arrayOfNotPizzaObject[j].src,
+      arrayOfNotPizzaObject[j].clickedOn,
+      arrayOfNotPizzaObject[j].views
+    );
+  }
+} else {
+  new Pizza('Papa Vito\'s Thin', 'images/mwDeluxePizzaThinCrust.jpg');
+  new Pizza('Chicago Deep Dish', 'images/chicagoPizza.jpg');
+  new Pizza('Brick Oven Pizza', 'images/brickOvenPizza.jpg');
+  new Pizza('Calzone', 'images/calzonePizza.jpg');
+  new Pizza('Chicago Pizza and Oven Grinder', 'images/cpoGinderPizza.jpg');
+  new Pizza('Detroit Style', 'images/detroitPizza.jpg');
+  new Pizza('New York Thin', 'images/newYorkPizza.jpg');
+  new Pizza('Shot Gun Dans', 'images/sgDansHtossedMeatLovPizza.jpg');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function getRandomNumber(){
   return Math.floor(Math.random() * Pizza.allPizzasArray.length);
@@ -83,6 +142,7 @@ function handlePizzaClick(event){
     pizzaContainer.removeEventListener('click', handlePizzaClick);
     // resultButton.addEventListener('click', renderResults);
     pizzaContainer.className = 'no-voting';
+    localStorage.setItem('savedPizza', JSON.stringify(Pizza.allPizzasArray));
     renderChart();
   } else {
     renderPizzas();
@@ -111,103 +171,53 @@ function renderChart(){
   https://www.chartjs.org/docs/latest/getting-started/ */
 
 
-
-
-  const ctx = document.getElementById('myChart').getContext('2d');
-  const myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-      labels: pizzaNames,
-      datasets: [{
-        label: 'Likes',
-        data: pizzaLikes,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
+  const data = {
+    labels: pizzaNames,
+    datasets: [{
+      label: 'Likes',
+      data: pizzaLikes,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.3)',
+        'rgba(255, 206, 86, 0.9)',
+        'rgba(75, 192, 192, 0.7)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(5, 159, 64, 0.8)',
+        'rgba(5, 159, 64, 0.5)',
+        'rgba(5, 159, 64, 0.3)'
+      ],
+      borderColor: [
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
     },
+    {
+      label: 'Views',
+      data: pizzaViews,
+      backgroundColor: ['rgba(255, 159, 64, 0.3)'],
+      borderColor: ['rgba(255, 159, 64, 0.3)'],
+      borderWidth: 1
+    }]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
     options: {
       scales: {
         y: {
           beginAtZero: true
         }
       }
-    }
-  });
-
+    },
+  };
+    //go grab the canvas element from the html
+  const canvasChart = document.getElementById('myChart').getContext('2d');
+  // eslint-disable-next-line no-undef
+  new Chart(canvasChart, config);
 }
 
 
-//   const data = {
-//     labels: pizzaNames,
-//     datasets: [{
-//       label: 'Likes',
-//       data: pizzaLikes,
-//       backgroundColor: [
-//         'rgba(255, 99, 132, 0.2)',
-//         'rgba(54, 162, 235, 0.3)',
-//         'rgba(255, 206, 86, 0.9)',
-//         'rgba(75, 192, 192, 0.7)',
-//         'rgba(153, 102, 255, 0.2)',
-//         'rgba(5, 159, 64, 0.8)',
-//         'rgba(5, 159, 64, 0.5)',
-//         'rgba(5, 159, 64, 0.3)'
-//       ],
-//       borderColor: [
-//         'rgba(255, 159, 64, 1)'
-//       ],
-//       borderWidth: 1
-//     },
-//     {
-//       label: 'Views',
-//       data: pizzaViews,
-//       backgroundColor: ['rgba(255, 159, 64, 0.3)'],
-//       borderColor: ['rgba(255, 159, 64, 0.3)'],
-//       borderWidth: 1
-//     }]
-//   };
-
-//   const config = {
-//     type: 'bar',
-//     data: data,
-//     options: {
-//       scales: {
-//         y: {
-//           beginAtZero: true
-//         }
-//       }
-//     },
-//   };
-//     //go grab the canvas element from the html
-//   const canvasChart = document.getElementById('myChart').getContext('2d');
-//   // eslint-disable-next-line no-undef
-//   new Chart(canvasChart, config);
-// }
-
-
-
-// function renderResults(){
-// let ul = document.querySelector('ul');
-// for(let i = 0; i < Pizza.allPizzasArray.length; i++){
-//   let li = document.createElement('li');
-//   li.textContent = `${Pizza.allPizzasArray[i].name} had ${Pizza.allPizzasArray[i].views} views and was clicked on ${Pizza.allPizzasArray[i].clickedOn} times`;
-//   ul.appendChild(li);
-// }
-// }
 
 
 
@@ -222,25 +232,6 @@ function renderChart(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-console.log(Pizza.allPizzasArray);
-new Pizza('Brick Oven Pizza', 'images/brickOvenPizza.jpg');
-new Pizza('Calzone', 'images/calzonePizza.jpg');
-new Pizza('Chicago Deep Dish', 'images/chicagoPizza.jpg');
-new Pizza('Chicago Pizza and Oven Grinder', 'images/cpoGinderPizza.jpg');
-new Pizza('Detroit Style', 'images/detroitPizza.jpg');
-new Pizza('Papa Vito\'s Thin', 'images/mwDeluxePizzaThinCrust.jpg');
-new Pizza('New York Thin', 'images/newYorkPizza.jpg');
-new Pizza('Shot Gun Dans Pizza', 'images/sgDansHtossedMeatLovPizza.jpg');
 //starts here with two images ready for click
 renderPizzas();
 
