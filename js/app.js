1edece40bb0c5bd757de6c0db591c7b8f522613a
tektechnologies <info@tektechnologies.com>
2022-04-13 21:16:28 -0500

    update image names and complete local storage

--- js/app.js
+++ js/app.js
@@ -19,13 +19,13 @@ console.log('pizza js connected');
 
 //Global variables
 const pizzaNames = [
-  'Papa Vito\'s Thin','Chicago Deep Dish',
-  'Brick Oven Pizza',
+  'Papa-Vitos-Thin','Chicago-Deep-Dish',
+  'Brick-Oven-Pizza',
   'Calzone',
-  'Chicago Pizza and Oven Grinder',
-  'Detroit Style',
-  'New York Thin',
-  'Shot Gun Dans'];
+  'Chicago-Pizza-and-Oven-Grinder',
+  'Detroit-Style',
+  'New-York-Thin',
+  'Shot-Gun-Dans'];
 
 let correctPizza = '';
 let wrongPizza = '';
@@ -34,24 +34,147 @@ const maxAttempts = 10;
 
 
 // get Dom elements from html
+const pizzaNameElement = document.getElementById('pizzaName');
+const scoreElement = document.getElementById('score');
+const attemptsElement = document.getElementById('attempts');
+const pizzaImagesParent = document.getElementById('pizzaImages');
+const responseElement = document.getElementById('response');
 
 
 
 //create set up function
 
+function setup(){
+//random name
+//1.
+  correctPizza = generateRandomPizza();
+  //2.
+  wrongPizza = generateRandomPizza();
+  //3.
+  updatePizzaName(correctPizza);
+
+
+  if(attempts){
+    pizzaImagesParent.removeChild(pizzaImagesParent.lastChild);
+    pizzaImagesParent.removeChild(pizzaImagesParent.lastChild);
+  }
+
+  //need render once we have random image
+  //4.
+  renderPizzaImage(correctPizza);
+  //5.
+  renderPizzaImage(wrongPizza);
+  //update the score
+  //6.
+  updateScoreElement();
+  //update attempts
+  //7.
+  updateAttempts();
+
+}
+setup();
+
+
+
+
+
+
+
+
 //creating a generate random pizza function
+
+function generateRandomPizza(){
+  const index = Math.floor(Math.random() * pizzaNames.length);
+  return pizzaNames[index];
+}
+
+
+function updatePizzaName(pizzaName){
+  pizzaNameElement.textContent = pizzaName;
+}
+
+function renderPizzaImage(pizzaName){
+  const img = document.createElement('img');
+  img.setAttribute('src', 'images/' + pizzaName + '.jpg');
+  img.setAttribute('id', pizzaName);
+  pizzaImagesParent.append(img);
+}
+
+
+//render a respone
+function renderResponse(response){
+  responseElement.textContent = response;
+}
 
 
 //update pizza name
-
-
-
 //render pizza images
-
-
 //event listener to render out our respone.
 
+pizzaImagesParent.addEventListener('click', function(event){
+  if(attempts === maxAttempts){
+    return;
+  }
+  const anwser = event.target.getAttribute('id');
+
+  if(anwser === correctPizza){
+    incrementScore();
+    renderResponse('whoo hoo, ZA you right you know your pizza!');
+  } else {
+    renderResponse('No thats not the correct pizza name for the image! You should eat more pizza.');
+  }
+  attempts++;
+  setup();
+
+  // if(attempts === maxAttempts){
+  //   draw();
+  //   renderList
+  // }
+});
+
+
+
 //increment
+function incrementScore(){
+  let score = getScore();
+  score++;
+  createorUpdateScore(score);
+  updateScoreElement();
+}
+
+
+
+function getScore(){
+  let score = localStorage.getItem('scoreValue');
+  if(score !== null){
+    score = parseInt(score);
+  }
+  return score;
+}
+
+function createorUpdateScore(scoreValue){
+  //  scoreValue =  scoreValue.toString();
+  scoreValue = JSON.stringify(scoreValue);
+  localStorage.setItem('scoreValue', scoreValue);
+  const score = localStorage.getItem('scoreValue');
+  return score;
+}
+
+
+
+
+function updateScoreElement(){
+  scoreElement.textContent = getScore() || 0;
+}
+
+function updateAttempts(){
+  attemptsElement.textContent = maxAttempts - attempts;
+}
+
+
+
+
+
 
 //delete score
 
