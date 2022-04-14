78e7e985b17e8aa761e18de9bd71c2c3ed7f832b
tektechnologies <info@tektechnologies.com>
2022-04-13 20:22:38 -0500

    set up js file

--- js/app.js
+++ js/app.js
@@ -1,16 +1,64 @@
 'use strict';
 console.log('pizza js connected');
+//Create an array of pizza objects, have a file for them.
+//dont repeat the correct picture
+//randomize the pizza image placement
+//track a score - score should increment by 1 when the user guess the correct image.
+// TODO a way to track how many attempts they've used - increment attempt in the event handler when any image is clicked
+// TODO some way to play again without having to refresh
+// TODO a way to stop the event listener from doing its thing when the num of attempts reaches a maximum number
+// TODO when they reach the max, show a chart with the score and numAttempts
 
-//global variables
+// Persistence of data
+// When we persist data, we need to be able to do four things with it:
+// create the data - setItem in localStorage
+// retrieve it
+// update it - setItem in localStorage
+// delete it
 
-  'Papa Vito\'s Thin', 
-  'Chicago Deep Dish', 
-  'Brick Oven Pizza', 
-  'Calzone', 
-  'Chicago Pizza and Oven Grinder', 
-  'Detroit Style', 
-  'New York Thin', 
-  'Shot Gun Dans'
+
+//Global variables
+const pizzaNames = [
+  'Papa Vito\'s Thin','Chicago Deep Dish',
+  'Brick Oven Pizza',
+  'Calzone',
+  'Chicago Pizza and Oven Grinder',
+  'Detroit Style',
+  'New York Thin',
+  'Shot Gun Dans'];
+
+let correctPizza = '';
+let wrongPizza = '';
+let attempts = 0;
+const maxAttempts = 10;
+
+
+// get Dom elements from html
+
+
+
+//create set up function
+
+//creating a generate random pizza function
+
+
+//update pizza name
+
+
+
+//render pizza images
+
+
+//event listener to render out our respone.
+
+//increment
+
+//delete score
+
+
+
+
+
 
 
 
